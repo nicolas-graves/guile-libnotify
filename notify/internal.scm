ea0c82f4fcf31d92e8bac15919330ae2e703a42d
Ekaitz Zarraga <ekaitz@elenq.tech>
2024-08-18 18:24:47 +0200

    move library load to separate expression

--- notify/internal.scm
+++ notify/internal.scm
@@ -18,70 +18,72 @@
   #:use-module (system foreign-library)
   #:use-module (system foreign-object))
 
-;; libnotify functions
+(define libnotify (load-foreign-library "libnotify"))
+(define glib      (load-foreign-library "libglib-2.0"))
 
+;; libnotify functions
 (define-public notify-init
-  (foreign-library-function "libnotify" "notify_init"
+  (foreign-library-function libnotify "notify_init"
                             #:return-type int
                             #:arg-types (list '*)))
 (define-public notify-uninit
-  (foreign-library-function "libnotify" "notify_uninit"
+  (foreign-library-function libnotify "notify_uninit"
                             #:return-type void
                             #:arg-types (list)))
 (define-public notify-notification-new
-  (foreign-library-function "libnotify" "notify_notification_new"
+  (foreign-library-function libnotify "notify_notification_new"
                             #:return-type '*
                             #:arg-types (list '* '* '*)))
 
 (define-public notify-notification-show
-  (foreign-library-function "libnotify" "notify_notification_show"
+  (foreign-library-function libnotify "notify_notification_show"
                             #:return-type int
                             #:arg-types (list '* '*)))
 
 (define-public notify-notification-update
-  (foreign-library-function "libnotify" "notify_notification_update"
+  (foreign-library-function libnotify "notify_notification_update"
                             #:return-type int
                             #:arg-types (list '* '* '* '*)))
 
 (define-public notify-notification-close
-  (foreign-library-function "libnotify" "notify_notification_close"
+  (foreign-library-function libnotify "notify_notification_close"
                             #:return-type int
                             #:arg-types (list '* '*)))
 
 ; From 0.8.4
 ; (define-public notify-notification-set-app-icon
-;   (foreign-library-function "libnotify" "notify_notification_set_app_icon"
+;   (foreign-library-function libnotify "notify_notification_set_app_icon"
 ;                             #:return-type void
 ;                             #:arg-types (list '* '*)))
 
 (define-public notify-notification-set-app-name
-  (foreign-library-function "libnotify" "notify_notification_set_app_name"
+  (foreign-library-function libnotify "notify_notification_set_app_name"
                             #:return-type void
                             #:arg-types (list '* '*)))
 
 (define-public notify-notification-set-category
-  (foreign-library-function "libnotify" "notify_notification_set_category"
+  (foreign-library-function libnotify "notify_notification_set_category"
                             #:return-type void
                             #:arg-types (list '* '*)))
 
 (define-public notify-notification-set-hint
-  (foreign-library-function "libnotify" "notify_notification_set_hint"
+  (foreign-library-function libnotify "notify_notification_set_hint"
                             #:return-type void
                             #:arg-types (list '* '* '*)))
 
 (define-public notify-notification-set-image-from-pixbuf
-  (foreign-library-function "libnotify" "notify_notification_set_image_from_pixbuf"
+  (foreign-library-function libnotify "notify_notification_set_image_from_pixbuf"
                             #:return-type void
                             #:arg-types (list '* '*)))
 
 (define-public notify-notification-set-timeout
-  (foreign-library-function "libnotify" "notify_notification_set_timeout"
+  (foreign-library-function libnotify "notify_notification_set_timeout"
                             #:return-type void
                             ;; Timeout can be NOTIFY_EXPIRES_DEFAULT / NOTIFY_EXPIRES_NEVER
                             #:arg-types (list '* int)))
 
 (define-public notify-notification-set-urgency
-  (foreign-library-function "libnotify" "notify_notification_set_urgency"
+  (foreign-library-function libnotify "notify_notification_set_urgency"
                            ; NOTIFY_URGENCY_LOW 0
                            ; NOTIFY_URGENCY_NORMAL 1
                            ; NOTIFY_URGENCY_CRITICAL 2
@@ -90,19 +92,19 @@
 
 
 (define-public notify-notification-clear-hints
-  (foreign-library-function "libnotify" "notify_notification_clear_hints"
+  (foreign-library-function libnotify "notify_notification_clear_hints"
                             #:return-type void
                             #:arg-types (list '*)))
 
 (define-public notify-notification-get-closed-reason
-  (foreign-library-function "libnotify" "notify_notification_get_closed_reason"
+  (foreign-library-function libnotify "notify_notification_get_closed_reason"
                             #:return-type int
                             #:arg-types (list '*)))
 
 ;; For the actions, we need function pointers
 ;; https://www.gnu.org/software/guile/manual/guile.html#index-procedure_002d_003epointer
 (define-public notify-notification-add-action
-  (foreign-library-function "libnotify" "notify_notification_add_action"
+  (foreign-library-function libnotify "notify_notification_add_action"
                             #:return-type void
                             #:arg-types (list '* '* '* '* '*)))
 
@@ -114,28 +116,28 @@
 
 ;; TODO: Implement iiibiiay
 (define g-variant-new
-  (foreign-library-function "libglib-2.0" "g_variant_new"
+  (foreign-library-function glib "g_variant_new"
                             #:return-type '*
                             #:arg-types (list '* '*)))
 (define g-variant-new-boolean
-  (foreign-library-function "libglib-2.0" "g_variant_new_boolean"
+  (foreign-library-function glib "g_variant_new_boolean"
                             #:return-type '*
                             #:arg-types (list int)))
 (define g-variant-new-byte
-  (foreign-library-function "libglib-2.0" "g_variant_new_byte"
+  (foreign-library-function glib "g_variant_new_byte"
                             #:return-type '*
                             #:arg-types (list uint8)))
 (define g-variant-new-int
-  (foreign-library-function "libglib-2.0" "g_variant_new_int32"
+  (foreign-library-function glib "g_variant_new_int32"
                             #:return-type '*
                             #:arg-types (list int)))
 (define g-variant-new-string
-  (foreign-library-function "libglib-2.0" "g_variant_new_string"
+  (foreign-library-function glib "g_variant_new_string"
                             #:return-type '*
                             #:arg-types (list '*)))
 
 (define g-variant-unref
-  (foreign-library-function "libglib-2.0" "g_variant_unref"
+  (foreign-library-function glib "g_variant_unref"
                             #:return-type void
                             #:arg-types (list '*)))
 

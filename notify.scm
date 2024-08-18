8d213568b42fcd96ac547d4570ffa3874f6820a6
Ekaitz Zarraga <ekaitz@elenq.tech>
2024-08-18 21:15:14 +0200

    Export variables

--- notify.scm
+++ notify.scm
@@ -1,41 +1,56 @@
 (define-module (notify)
   #:use-module (oop goops)
   #:use-module (system foreign)
-  #:use-module (notify internal)
-  #:use-module (system foreign-object))
+  #:use-module ((notify internal) #:prefix internal:)
+  #:use-module (system foreign-object)
+  #:export (notify-init
+            notify-uninit
+            notification-new
+            notification-update
+            notification-show
+            notification-close
+            notification-set-app-name
+            notification-set-category
+            notification-set-image-from-pixbuf
+            notification-set-timeout
+            notification-set-urgency
+            notification-set-hint
+            notification-clear-hints
+            notification-get-closed-reason
+            notification-add-action))
 
 (define-syntax or-NULL
   (syntax-rules ()
     ((_ value conv-f) (if value (conv-f value) %null-pointer))
     ((_ value)        (or value %null-pointer))))
 
-(define* (init #:key (app-name #f))
-  (if (= 0 (notify-init (or-NULL app-name string->pointer)))
+(define* (notify-init #:key (app-name #f))
+  (if (= 0 (internal:notify-init (or-NULL app-name string->pointer)))
     (error "notify-init failed")))
 
-(define (uninit)
-  (notify-uninit))
+(define (notify-uninit)
+  (internal:notify-uninit))
 ;; TODO: add call-with-notify ?
 
 (define* (notification-new summary #:key (body #f) (icon #f))
-  (notify-notification-new (string->pointer summary)
-                           (or-NULL body string->pointer)
-                           (or-NULL icon string->pointer)))
+  (internal:notify-notification-new (string->pointer summary)
+                                    (or-NULL body string->pointer)
+                                    (or-NULL icon string->pointer)))
 
 (define* (notification-update summary #:key (body #f) (icon #f))
-  (notify-notification-update (string->pointer summary)
-                              (or-NULL body string->pointer)
-                              (or-NULL icon string->pointer)))
+  (internal:notify-notification-update (string->pointer summary)
+                                       (or-NULL body string->pointer)
+                                       (or-NULL icon string->pointer)))
 
 
 (define (notification-show notification)
   ;; TODO send the GError** as second parameter and deal with it
-  (if (= 0 (notify-notification-show notification %null-pointer))
+  (if (= 0 (internal:notify-notification-show notification %null-pointer))
     (error "notify-notification-show: error happened")))
 
 (define (notification-close notification)
   ;; TODO send the GError** as second parameter and deal with it
-  (if (= 0 (notify-notification-close notification %null-pointer))
+  (if (= 0 (internal:notify-notification-close notification %null-pointer))
     (error "notify-notification-close: error happened")))
 
 ; From 0.8.4
@@ -43,14 +58,17 @@
 ;  (notify-notification-set-app-icon notification (string->pointer icon)))
 
 (define (notification-set-app-name notification name)
-  (notify-notification-set-app-name notification (string->pointer name)))
+  (internal:notify-notification-set-app-name notification
+                                             (string->pointer name)))
 
 (define (notification-set-category notification category)
-  (notify-notification-set-category notification (string->pointer category)))
+  (internal:notify-notification-set-category notification
+                                             (string->pointer category)))
 
 ;; TODO: maybe remove me
 (define (notification-set-image-from-pixbuf notification gdk-pixbuf-image)
-  (notify-notification-set-image-from-pixbuf notification gdk-pixbuf-image))
+  (internal:notify-notification-set-image-from-pixbuf notification
+                                                      gdk-pixbuf-image))
 
 (define (notification-set-timeout notification timeout)
   (define (timeout->num timeout)
@@ -58,7 +76,8 @@
       ('never 0)
       ('default -1)
       (else timeout)))
-  (notify-notification-set-timeout notification (timeout->num timeout)))
+  (internal:notify-notification-set-timeout notification
+                                            (timeout->num timeout)))
 
 (define (notification-set-urgency notification urgency)
   (define (urgency->num urgency)
@@ -66,7 +85,8 @@
       ('low      0)
       ('normal   1)
       ('critical 2)))
-  (notify-notification-set-urgency notification (urgency->num urgency)))
+  (internal:notify-notification-set-urgency notification
+                                            (urgency->num urgency)))
 
 
 
@@ -88,25 +108,28 @@
       ('urgency        'byte)
       (else            (error "Unknown hint" key))))
 
-  (let ((variant (make-g-variant (key-type key) value)))
-    (notify-notification-set-hint notification
-                                  (string->pointer (symbol->string key))
-                                  (make-pointer
-                                    (slot-ref variant 'var)))))
+  (let ((variant (internal:make-g-variant (key-type key) value)))
+    (internal:notify-notification-set-hint
+      notification
+      (string->pointer (symbol->string key))
+      (make-pointer
+        (slot-ref variant 'var)))))
 
-(define notification-clear-hints notify-notification-clear-hints)
+(define notification-clear-hints
+  internal:notify-notification-clear-hints)
 
-(define notification-get-closed-reason notify-notification-get-closed-reason)
+(define notification-get-closed-reason
+  internal:notify-notification-get-closed-reason)
 
 (define* (notification-add-action . things)
   (error "Unimplemented"))
 
 
 ;; How to use the higher level interface
-;; (init #:app-name "notify.scm")
+;; (notify-init #:app-name "notify.scm")
 ;; (define noti (notification-new "Problem!" #:body "Device died!"))
 ;; (notification-set-category noti "device.error")
 ;; (notification-set-hint noti 'image-path "file:///home/Ekaitz/clip.png")
 ;; (notification-set-hint noti 'urgency 3)
 ;; (notification-show noti)
-;; (uninit)
+;; (notify-uninit)
